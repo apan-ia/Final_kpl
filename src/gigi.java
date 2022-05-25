54cd504a5392fdb21072e2678c9bd3a5e1d7821d
apan-ia <82051942+apan-ia@users.noreply.github.com>
2022-05-26 00:02:24 +0700

    Add files via upload

---
+++ src/gigi.java
@@ -0,0 +1,34 @@
+import java.net.MalformedURLException;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class gigi extends base{
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		
+		
+		AndroidDriver<AndroidElement> driver =capabilities();
+		
+		/*
+		 * 1. Xpath
+		 * 
+		 * syntax 
+		 *   //tagname[@id='value']
+		 *   
+		 * 
+		
+		*driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+		*driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+		**/
+
+		/*
+		 * 2. id dan index
+		 * 
+		 * */
+		
+		driver.findElementById("com.application.app:id/btnReservasi").click();
+		driver.findElementByXPath("//android.widget.Button[@text='Masuk']").click();
+		driver.findElementById("com.application.app:id/linearGroup292").click();
+	}
+}
---
+++ src/reservasi.java
@@ -0,0 +1,35 @@
+import java.net.MalformedURLException;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class reservasi extends base{
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		
+		
+		AndroidDriver<AndroidElement> driver =capabilities();
+		
+		/*
+		 * 1. Xpath
+		 * 
+		 * syntax 
+		 *   //tagname[@id='value']
+		 *   
+		 * 
+		
+		*driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+		*driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+		**/
+
+		/*
+		 * 2. id dan index
+		 * 
+		 * */
+		
+		driver.findElementById("com.application.app:id/btnReservasi").click();
+		driver.findElementByXPath("//android.widget.Button[@text='Masuk']").click();
+		driver.findElementByXPath("//android.widget.ImageView[@id='imageClipboardr']").click();
+		
+	}
+}
